728d9994d9253f358b8e34da0f3f34df663758c4
Robert_Jean <stevejean285@gmail.com>
2024-12-15 23:27:19 -0500

    Installed and Tested Chakra UI to design the front end interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/Signin';
-import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/signin' element={<SignIn/>}/>
-      <Route path='/signUp' element={<SignUp/>}/>
-
-
-    </Routes>
-    </BrowserRouter>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+   );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import { ChakraProvider } from "@chakra-ui/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ChakraProvider>
     <App />
-  </StrictMode>,
-)
+    </ChakraProvider>
+  </StrictMode>
+);
