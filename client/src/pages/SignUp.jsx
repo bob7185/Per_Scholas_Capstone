367926037d0bb8f7b05fc1c81ce7978e3b4d4c88
Robert_Jean <stevejean285@gmail.com>
2024-12-16 00:28:38 -0500

    Created Basic form for sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,95 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import {
+  FormControl,
+  Input,
+  Button,
+  Text,
+  Box,
+  Flex,
+  Heading,
+  Stack,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+
 export default function SignUp() {
-    return <h1>Dashboard</h1>
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const submitForm = () => alert("Sign up succesful. You are now logged in");
+
+  return (
+    <Box p="4" maxW="lg" mx="auto">
+      <Heading
+        as="h1"
+        textAlign="center"
+        fontSize="3xl"
+        fontWeight="semibold"
+        my="7"
+      >
+        {" "}
+        Create an Account
+      </Heading>
+      <form>
+        <Stack gap="4">
+          {/* Username field input */}
+          <FormControl isInvalid={errors.username}>
+            <Input
+              id="username"
+              type="text"
+              placeholder="username"
+              {...register("username", { required: "Username is required" })}
+            />
+            <FormErrorMessage>
+              {errors.username && errors.username.message}
+            </FormErrorMessage>
+          </FormControl>
+          {/* Email input  */}
+          <FormControl isInvalid={errors.email}>
+            <Input
+              id="email"
+              type="text"
+              placeholder="email"
+              {...register("email", { required: "Email is required" })}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+
+          {/* password field input */}
+          <FormControl isInvalid={errors.password}>
+            <Input
+              id="password"
+              type="password"
+              placeholder="password"
+              {...register("password", { required: "Password is required" })}
+            />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button
+            type="submit"
+            isLoading={isSubmitting}
+            colorScheme="teal"
+            textTransform="uppercase"
+          >
+            Sign Up
+          </Button>
+        </Stack>
+      </form>
+      <Flex gap="2" mt="5">
+        <Text>Have an account?</Text>
+        <Link to={"/signin"}>
+          <Text as="span" color="blue.400">
+            Sign in
+          </Text>
+        </Link>
+      </Flex>
+    </Box>
+  );
 }
