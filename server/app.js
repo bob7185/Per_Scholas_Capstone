ac9e9b2e3dd6dcd01f685010191f3b7e500b33a2
Robert_Jean <stevejean285@gmail.com>
2024-12-19 09:14:09 -0500

    pushing production built

--- server/app.js
+++ server/app.js
@@ -4,9 +4,18 @@ import usersRouter from './routes/users.js';
 import tasksRouter from './routes/tasks.js';
 import authRouter from './routes/auth.js'
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 const PORT = 8000;
 
 app.use(express.json());
@@ -22,9 +31,14 @@ app.use('/tasks', tasksRouter);
 app.use('/auth', authRouter)
 
 // Catch-all route for undefined endpoints
-app.use('*', (req, res) => {
-    res.status(404).json({ message: 'Endpoint not found' });
-});
+// app.use('*', (req, res) => {
+//     res.status(404).json({ message: 'Endpoint not found' });
+// });
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening at port: ${PORT}`);
